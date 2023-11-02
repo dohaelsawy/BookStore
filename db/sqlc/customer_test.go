067d244c955ada1bd8e7462fc23e5fcdc93e43fc
dohaelsawy <dohaelsawy18@gmail.com>
2023-11-02 14:31:41 +0200

    generating test functions for customer db with random values

---
+++ db/sqlc/customer_test.go
@@ -0,0 +1,64 @@
+package db
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/dohaelsawy/bookStore/util"
+)
+
+func TestCustomerCreating(t *testing.T) {
+	arg := CreatecustomerParams{
+		FirstName: util.RandomString(6),
+		LastName: util.RandomString(6),
+		Email: util.RandomEmail(),
+		Password: util.RandomPssword(),
+		City: "cairo",
+		PhoneNumber: util.RandomPhoneNumber(),
+	}
+	customer , err := testDB.Createcustomer(context.Background(), arg)
+	require.NoError(t , err)
+	require.NotEmpty(t, customer)
+	require.Equal(t, arg.FirstName, customer.FirstName)
+	require.Equal(t, arg.LastName, customer.LastName)
+	require.Equal(t, arg.Email,customer.Email)
+	require.Equal(t, arg.Password, customer.Password)
+	require.Equal(t, arg.City, customer.City)
+	require.Equal(t, arg.PhoneNumber, customer.PhoneNumber)
+	require.NotZero(t , customer.CustomerID)
+}
+
+func TestCustomerUpdating(t *testing.T) {
+	arg := UpdatecustomerParams{
+		CustomerID: 3,
+		FirstName: util.RandomString(6),
+		LastName: util.RandomString(6),
+		Email: util.RandomEmail(),
+		Password: util.RandomPssword(),
+		City: "cairo",
+		PhoneNumber: util.RandomString(8),
+	}
+	customer , err := testDB.Updatecustomer(context.Background(), arg)
+	require.NoError(t , err)
+	require.NotEmpty(t, customer)
+	require.Equal(t, arg.FirstName, customer.FirstName)
+	require.Equal(t, arg.LastName, customer.LastName)
+	require.Equal(t, arg.Email,customer.Email)
+	require.Equal(t, arg.Password, customer.Password)
+	require.Equal(t, arg.City, customer.City)
+	require.Equal(t, arg.PhoneNumber, customer.PhoneNumber)
+	require.NotZero(t , customer.CustomerID)
+}
+
+func TestCustomerDeleting(t *testing.T) {
+	err := testDB.Deletecustomer(context.Background(), 1)
+	require.NoError(t , err)
+}
+
+func TestCustomerAllList(t *testing.T){
+	customers , err := testDB.ListCustomers(context.Background())
+	require.NoError(t , err)
+	require.NotEmpty(t , customers)
+	require.NotZero(t , len(customers))
+}
--- util/random.go
+++ util/random.go
@@ -14,13 +14,13 @@ func randomNumber(min int64 , max int64) int64{
 }
 
 const alphapet = "abcdefghijklmnopqrstuvwxyz"
+const nums = "1234567890"
 
-
-func randomString(n int) (string){
+func randomString(n int , Word string) (string){
 	var builder strings.Builder
-	k := len(alphapet)
+	k := len(Word)
 	for i := 0 ; i < n ;i++ {
-		c := alphapet[rand.Intn(k)]
+		c := Word[rand.Intn(k)]
 		builder.WriteByte(c)
 	}
 	return builder.String()
@@ -28,9 +28,27 @@ func randomString(n int) (string){
 
 
 func RandomString(length int) string {
-	return randomString(int(length))
+	return randomString(int(length) , alphapet)
 }
 
 func RandomNumber(min int64 , max int64) int64 {
 	return randomNumber(min , max)
+}
+
+func RandomEmail() string {
+	var builder strings.Builder
+	email:= "@gmail.com"
+	s := randomString(6 , alphapet)
+	builder.WriteString(s)
+	builder.WriteString(email)
+	return builder.String()
+}
+
+
+func RandomPssword() string {
+	return randomString(8 , alphapet)
+}
+
+func RandomPhoneNumber() string {
+	return randomString(11 , nums)
 }
